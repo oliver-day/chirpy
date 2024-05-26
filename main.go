8262ee2c2accb146ddd948379f4c8c5a5a4b4c27
oliver-day <oliver.day.swe@gmail.com>
2024-05-26 13:41:34 -0700

    feat: add explicit HTTP methods to endpoints

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,31 +9,21 @@ type apiConfig struct {
 	fileserverHits int
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
-}
-
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
-	apiCfg := &apiConfig{}
+	apiCfg := &apiConfig{
+		fileserverHits: 0,
+	}
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir(filePathRoot))
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.Handle("/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
-	mux.Handle("/reset", http.HandlerFunc(apiCfg.handlerReset))
+	fileServer := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.Handle("/app/*", fileServer)
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
