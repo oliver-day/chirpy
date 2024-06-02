336e942fd34d282bf50d877a00b933f47ba8e844
oliver-day <oliver.day.swe@gmail.com>
2024-06-01 23:06:49 -0700

    feat: update 'handlerUsersCreate' to hash user password

--- handler_users_create.go
+++ handler_users_create.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+
+	"github.com/oday/chirpy/internal/auth"
+	"github.com/oday/chirpy/internal/database"
 )
 
 type User struct {
-	ID    int    `json:"id"`
-	Email string `json:"email"`
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
 }
 
 // Methods --
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Email string `json:"email"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	type response struct {
+		User
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -24,14 +33,26 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	user, err := cfg.DB.CreateUser(params.Email)
+	hashedPassword, err := auth.HashPassword(params.Password)
 	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to hash password")
+		return
+	}
+
+	user, err := cfg.DB.CreateUser(params.Email, hashedPassword)
+	if err != nil {
+		if errors.Is(err, database.ErrorUserAlreadyExists) {
+			respondWithError(w, http.StatusConflict, "User already exists")
+			return
+		}
 		respondWithError(w, http.StatusInternalServerError, "Failed to create user")
 		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, User{
-		ID:    user.ID,
-		Email: user.Email,
+	respondWithJSON(w, http.StatusCreated, response{
+		User{
+			ID:    user.ID,
+			Email: user.Email,
+		},
 	})
 }
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HashedPassword(password string) (string, error) {
+func HashPassword(password string) (string, error) {
 	data, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
