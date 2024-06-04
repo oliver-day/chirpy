40b5a2b931fb3d327a2fad886d1799b0933d913c
oliver-day <oliver.day.swe@gmail.com>
2024-06-04 15:45:08 -0700

    feat: add 'RefreshToken.SaveRefreshToken' method

--- internal/database/refresh_tokens.go
+++ internal/database/refresh_tokens.go
@@ -7,3 +7,26 @@ type RefreshToken struct {
 	Token     string    `json:"token"`
 	ExpiresAt time.Time `json:"expires_at"`
 }
+
+// DBStructure.RefreshTokens Methods --
+
+func (db *DB) SaveRefreshToken(userID int, token string) error {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return err
+	}
+
+	refreshToken := RefreshToken{
+		UserID:    userID,
+		Token:     token,
+		ExpiresAt: time.Now().Add(time.Hour), // 1 hour
+	}
+	dbStructure.RefreshTokens[token] = refreshToken
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
