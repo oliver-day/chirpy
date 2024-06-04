3ceb65b159340222e79abf71d0d5fda0a7230e89
oliver-day <oliver.day.swe@gmail.com>
2024-06-04 16:07:22 -0700

    feat: add 'POST /api/refresh' handler

---
+++ handler_refresh.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/oday/chirpy/internal/auth"
+)
+
+func (cfg *apiConfig) handlerRefresh(w http.ResponseWriter, r *http.Request) {
+	type response struct {
+		Token string `json:"token"`
+	}
+
+	refreshToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to find refresh token in header")
+		return
+	}
+
+	user, err := cfg.DB.GetUserForRefreshToken(refreshToken)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Failed to get user for refresh token")
+		return
+	}
+
+	accessToken, err := auth.MakeJWT(user.ID, cfg.jwtSecret, time.Hour)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Failed validate JWT")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, response{
+		Token: accessToken,
+	})
+}
